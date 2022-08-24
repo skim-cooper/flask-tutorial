35d1b9acce1417f1fb130e803193d12411adb2f7
Steve Kim <skim@cooperequipment.ca>
2022-08-24 15:04:32 -0400

    Follow first two sections of Flask tutorial 1. Project Layout: https://flask.palletsprojects.com/en/1.1.x/tutorial/layout/ 2. Application Setup: https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/

---
+++ flaskr/__init__.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    # __name__ is the name of the current Python module.
+    # instance_relative_config=True tells the app that configuration
+    # files are relatie to the instance folder. The instance folder
+    # is located outside the flaskr package and can hold local data
+    # that shouldn't be commited to version control, such as configuration
+    # secrets and the database file.
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    # app.config.from_mapping() sets some default configuration that the app will use
+
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+
