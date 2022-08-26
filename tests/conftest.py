5618432a6786e74b7fc249829c0bbf2e087cc788
Steve Kim <skim@cooperequipment.ca>
2022-08-26 17:23:00 -0400

    Flask tutorial section 8 Test Coverage

---
+++ tests/conftest.py
@@ -0,0 +1,64 @@
+# Setup test configuration instead of using local configuration
+
+import os
+import tempfile
+
+import pytest
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+# The client fixture calls app.test_client() with the application object 
+# created by the app fixture. Tests will use the client to make requests 
+# to the application without running the server.
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+# The runner fixture is similar to client. 
+# app.test_cli_runner() creates a runner that can call the Click commands 
+# registered with the application.
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
